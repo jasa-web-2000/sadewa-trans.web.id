35759c412ed5e100cac59b36cca6862810eca2f0
dion-zebua <zebuadbless@gmail.com>
2025-05-19 19:28:19 +0700

    malas ngoding, paham 🖐

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $logoPanjang = 'src/img/sadewa.png';
 $title = $brand . " Travel Jawa Tengah Terbaik No. 1";
 $desc = $brand . " menawarkan jasa travel dengan harga murah dan jadwal 24 jam";
 $telp = '0882-0068-91597';
-// $telp2 = '0822-4108-1699';
+$telp2 = '0822-4171-2221';
 $alamat = 'Kedunggading, Kabupaten Kendal, Jawa Tengah';
 $iframe = 'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3960.0656667972175!2d110.11031557499723!3d-7.001549692999713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zN8KwMDAnMDUuNiJTIDExMMKwMDYnNDYuNCJF!5e0!3m2!1sid!2sid!4v1747653996542!5m2!1sid!2sid';
 
